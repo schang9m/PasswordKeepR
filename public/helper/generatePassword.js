2805265d88222a8ed07715fca5e3a6fbccae9877
Sampson Chang <sampson.chang@gmail.com>
2024-07-28 18:14:34 -0700

    made generate password works on browser

--- public/helper/generatePassword.js
+++ public/helper/generatePassword.js
@@ -31,13 +31,16 @@ function generatePassword(length = 6, options = { lower: true, upper: true, numb
     const randomIndex = Math.floor(Math.random() * charactersLength);
     password += characters.charAt(randomIndex);
   }
-
   return password;
 }
 
 // Example usage:
-console.log(generatePassword()); // Default length 6 with all options enabled
-console.log(generatePassword(8)); // Length 8 with all options enabled
-console.log(generatePassword(12, { lower: true, upper: true, numbers: true, special: true })); // Custom length and options
-console.log(generatePassword(10, { lower: true, upper: false, numbers: true, special: false })); // Custom length with selected options
-console.log(generatePassword(10, { lower: false, upper: false, numbers: false, special: false })); // Check if error is working
+// console.log(generatePassword()); // Default length 6 with all options enabled
+// console.log(generatePassword(8)); // Length 8 with all options enabled
+// console.log(generatePassword(12, { lower: true, upper: true, numbers: true, special: true })); // Custom length and options
+// console.log(generatePassword(10, { lower: true, upper: false, numbers: true, special: false })); // Custom length with selected options
+// console.log(generatePassword(10, { lower: false, upper: false, numbers: false, special: false })); // Check if error is working
+
+
+//export the helper function out
+module.exports = { generatePassword };
