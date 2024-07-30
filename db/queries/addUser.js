130a554867bfecf0b1ef6a6325b8105502b3de28
Sampson Chang <sampson.chang@gmail.com>
2024-07-29 17:23:32 -0700

    added into db successfully, with organization as placebo

--- db/queries/addUser.js
+++ db/queries/addUser.js
@@ -1,11 +1,23 @@
 const db = require('../connection');
 
 const addUser_Url = (user_url) => {
-  const {username, url, password, organization_id} = user_url
-  return db.query('INSERT INTO url_usernames(username, password, organization_id, url) VALUES ($1, $2, $3, $4)RETURNING *;'[username, url, password, 1])
-    .then(data => {
-      return data.rows;
-    });
+  const { username, url, password, organization_id } = user_url;
+
+  console.log('Adding user URL:', { username, url, password, organization_id });
+
+  return db.query(
+    'INSERT INTO url_usernames (username, url, password, organization_id ) VALUES ($1, $2, $3, $4) RETURNING *;',
+    [username, url, password, "1"]
+  )
+  //checking if it works
+    // .then(data => {
+    //   console.log('Insert successful:', data.rows);
+    //   return data.rows;
+    // })
+    // .catch(err => {
+    //   console.error('Error inserting user URL:', err);
+    //   throw err;
+    // });
 };
 
 module.exports = { addUser_Url };
