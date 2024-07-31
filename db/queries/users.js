c546c356e6daed02ef9464755575b3e01d262b13
Sampson Chang <sampson.chang@gmail.com>
2024-07-31 14:31:35 -0700

    added more info for the user tab

--- db/queries/users.js
+++ db/queries/users.js
@@ -2,7 +2,7 @@ const db = require('../connection');
 
 const getUsers = (userID) => {
   //getting all the users on the same org base on their user_id
-  return db.query('SELECT name FROM users WHERE organization_id = (SELECT organization_id FROM users WHERE id = $1);', [userID])
+  return db.query('SELECT * FROM users WHERE organization_id = (SELECT organization_id FROM users WHERE id = $1);', [userID])
     .then(data => {
       return data.rows;
     });
